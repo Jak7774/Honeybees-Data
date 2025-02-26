a5f264d36d7d6c229aa504d398dcb3168f02d76c
Jack Elkes <Jak7774@users.noreply.github.com>
2025-02-26 07:36:14 +0000

    JSON Post from Arduino

--- All_Sensors/All_Sensors.ino
+++ All_Sensors/All_Sensors.ino
@@ -24,15 +24,16 @@
 OneWire oneWire(ONE_WIRE_BUS); 
 DallasTemperature sensors(&oneWire);
 
-/* --- Seupt Humidity Sensors ---*/
-Adafruit_SHT31 sht31 = Adafruit_SHT31(); float temp[2]; float humid[2];
+/* --- Setup Humidity Sensors ---*/
+Adafruit_SHT31 sht31 = Adafruit_SHT31(); 
+float temp[2]; 
+float humid[2];
 
 /* Analogue Transmission of Data */
 void TCA9548A(uint8_t bus){
   Wire.beginTransmission(0x71);  // TCA9548A address is 0x70
   Wire.write(1 << bus);          // send byte to select bus
   Wire.endTransmission();
-  //Serial.print(bus);
 }
 
 /* --- Setup Scale Sensors --- */
@@ -44,44 +45,45 @@ HX711_ADC LoadCell(HX711_dout, HX711_sck);
 unsigned long t = 0;
 const int calVal_eepromAdress = 0;
 
+bool printRequest = false;
+
 /* -----------------------------
   Setup Sensors
   -----------------------------*/
 
 void setup(void) {
-  // Start Serial Communication for Debugging
   Serial.begin(9600);
-
+  Serial.flush();
   sensors.begin(); // temp sensors
-
-  // SHT31 Sensors
-  //sht31.begin(0x44);
   Wire.begin(); // I2C Multiplexer
   
-  if (! sht31.begin(0x44) && ! sht31.begin(0x45)) {   // Set to 0x45 for alternate i2c addr
+  if (!sht31.begin(0x44) && !sht31.begin(0x45)) {
     Serial.println("Couldn't find SHT31");
-    //while (1) delay(1);
   }
 
-  // Scales
-  float calibrationValue; // calibration value
-  //calibrationValue = 696.0; // uncomment this if you want to set this value in the sketch
+  float calibrationValue;
 #if defined(ESP8266) || defined(ESP32)
-  EEPROM.begin(512); // uncomment this if you use ESP8266 and want to fetch this value from eeprom
+  EEPROM.begin(512);
 #endif
-  EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch this value from eeprom
+  EEPROM.get(calVal_eepromAdress, calibrationValue);
 
   LoadCell.begin();
-  //LoadCell.setReverseOutput();
-  unsigned long stabilizingtime = 2000; // tare preciscion can be improved by adding a few seconds of stabilizing time
-  boolean _tare = false; //set this to false if you don't want tare to be performed in the next step
+  unsigned long stabilizingtime = 2000;
+  boolean _tare = false;
   LoadCell.start(stabilizingtime, _tare);
-  if (LoadCell.getTareTimeoutFlag()) {
-    //Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+  if (!LoadCell.getTareTimeoutFlag()) {
+    LoadCell.setCalFactor(calibrationValue);
   }
-  else {
-    LoadCell.setCalFactor(calibrationValue); // set calibration factor (float)
-    //Serial.println("Startup is complete");
+}
+
+/* -----------------------------
+   Check if serial data is available (request from Python)
+   -----------------------------*/
+   
+void checkSerial() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    printRequest = (command == 'P');
   }
 }
 
@@ -90,42 +92,33 @@ void setup(void) {
   -----------------------------*/
 
 void loop(void) {
-  // Call below to issue a global temp and request to all sensors
-  sensors.requestTemperatures();
-
-  // Temp Sensors
-  /* --- First Sensor --- */
-  //Serial.print("temp1");
-  Serial.print(sensors.getTempCByIndex(0));
-  Serial.print(", ");
-  /* --- Second Sensor --- */
-  Serial.print(sensors.getTempCByIndex(1));
-  Serial.print(", ");
-
-  // SHT31 Humidity Sensors
-  for (int i = 0; i < 2; i++) {
-    TCA9548A(i);
-    temp[i] = sht31.readTemperature();
-    humid[i] = sht31.readHumidity();
-    Serial.print(humid[i]);
-    Serial.print(", ");
-    Serial.print(temp[i]);
-    Serial.print(", ");
-  }
-
-  /* --- Weight Sensor Output --- */
-  static boolean newDataReady = 0;
-  if (LoadCell.update()) newDataReady = true;
-
-  // get smoothed value from the dataset:
-  if (newDataReady) {
-    if (millis() > t ) {
-      float w = LoadCell.getData();
-      Serial.print(w);
-      newDataReady = 0;
-      t = millis();
+  checkSerial();
+  if (printRequest) {
+    sensors.requestTemperatures();
+    float temp1 = sensors.getTempCByIndex(0);
+    float temp2 = sensors.getTempCByIndex(1);
+    
+    for (int i = 0; i < 2; i++) {
+      TCA9548A(i);
+      temp[i] = sht31.readTemperature();
+      humid[i] = sht31.readHumidity();
     }
+
+    static boolean newDataReady = false;
+    if (LoadCell.update()) newDataReady = true;
+    float weight = newDataReady ? LoadCell.getData() : 0.0;
+    
+    Serial.print("{");
+    Serial.print("\"temp1\": "); Serial.print(temp1, 2); Serial.print(", ");
+    Serial.print("\"temp2\": "); Serial.print(temp2, 2); Serial.print(", ");
+    Serial.print("\"humid1\": "); Serial.print(humid[0], 2); Serial.print(", ");
+    Serial.print("\"temp3\": "); Serial.print(temp[0], 2); Serial.print(", ");
+    Serial.print("\"humid2\": "); Serial.print(humid[1], 2); Serial.print(", ");
+    Serial.print("\"temp4\": "); Serial.print(temp[1], 2); Serial.print(", ");
+    Serial.print("\"weight\": "); Serial.print(weight, 2);
+    Serial.println("}");
+    
+    printRequest = false;
   }
-  Serial.println(" ");
-  delay(1000);
+  delay(1);
 }
